180cad8ad652f676154f934051ddafd040a93c10
A. I. LAILABA <abdullahiibrahima5126@gmail.com>
2026-01-17 07:47:30 +0100

    Updated

--- products.js
+++ products.js
@@ -1,13 +1,13 @@
  // Products list
 const products=[
-{id:1,name:"Shadda Newway 3 meter",price:25000,image:"images/shadda1.jpg"},
-{id:2,name:"Shadda swipt 3 meter",price:26000,image:"images/shadda2.jpg"},
-{id:3,name:"Yadi starro 5 yard",price:15000,image:"images/yadi1.jpg"},
-{id:4,name:"Roman Shoe",price:8000,image:"images/shoe1.jpg"},
-{id:5,name:"Hulla New style",price:14000,image:"images/hulla2.jpg"},
-{id:6,name:"Hulla Gure",price:15000,image:"images/hulla1.jpg"},
-{id:7,name:"Yadi starro 5 yard",price:15000,image:"images/yadi2.jpg"},
-{id:8,name:"Sandal Favil",price:7500,image:"images/shoe2.jpg"}
+{id:1,name:"Shadda Newway 3 meter",price:25000,image:"shadda1.jpg"},
+{id:2,name:"Shadda swipt 3 meter",price:26000,image:"shadda2.jpg"},
+{id:3,name:"Yadi starro 5 yard",price:15000,image:"yadi1.jpg"},
+{id:4,name:"Takalmi Roman Shoe",price:8000,image:"shoe1.jpg"},
+{id:5,name:"Hulla New style",price:14000,image:"hulla2.jpg"},
+{id:6,name:"Hulla Gure",price:15000,image:"hulla1.jpg"},
+{id:7,name:"Yadi starro 5 yard",price:15000,image:"yadi2.jpg"},
+{id:8,name:"Takalmi Sandal Favil",price:7500,image:"shoe2.jpg"}
 ];
 
 
